b7417e408ca289c7292e2173618fda663feb0e7d
Ivan Gasent <igaszar374@g.educaand.es>
2022-03-01 21:17:56 +0100

    update

--- src/inserts.js
+++ src/inserts.js
@@ -22,6 +22,142 @@ db.libros.insertMany([
         ],
         paginas: 192
     },
+    {
+        titulo: "Cronicas marcianas",
+        escritor: "Ray Bradbury",
+        fecha_publicacion: new Date("1950-1-1"),
+        precio: [
+            { formato: "tapa dura", precio: 24.95 },
+            { formato: "tapa blanda", precio: 7.95 },
+            { formato: "kindle", precio: 6.72 }
+        ],
+        paginas: 331
+    },
+    {
+        titulo: "Palabras radiantes",
+        coleccion: "El Archivo de las Tormentas",
+        escritor: "Brandon Sanderson",
+        fecha_publicacion: new Date("2014-3-4"),
+        precio: [
+            { formato: "tapa dura", precio: 34.90 },
+            { formato: "tapa blanda", precio: 14.95 },
+            { formato: "kindle", precio: 9.99 }
+        ],
+        paginas: 1248
+    },
+    {
+        titulo: "Juramentada",
+        coleccion: "El Archivo de las Tormentas",
+        escritor: "Brandon Sanderson",
+        fecha_publicacion: new Date("2017-11-14"),
+        precio: [
+            { formato: "tapa dura", precio: 34.90 },
+            { formato: "kindle", precio: 9.99 }
+        ],
+        paginas: 1408
+    },
+    {
+        titulo: "El Ritmo de la Guerra",
+        coleccion: "El Archivo de las Tormentas",
+        escritor: "Brandon Sanderson",
+        fecha_publicacion: new Date("2020-11-17"),
+        precio: [
+            { formato: "tapa dura", precio: 34.90 },
+            { formato: "kindle", precio: 12.99 }
+        ],
+        paginas: 1408
+    },
+    {
+        titulo: "Maquinas mortales",
+        coleccion: "Maquinas mortales",
+        escritor: "Philip Reeve",
+        fecha_publicacion: new Date("2001-11-16"),
+        precio: [
+            { formato: "tapa blanda", precio: 17.95 },
+            { formato: "kindle", precio: 7.99 }
+        ],
+        paginas: 368
+    },
+    {
+        titulo: "El oro del depredador",
+        coleccion: "Maquinas mortales",
+        escritor: "Philip Reeve",
+        fecha_publicacion: new Date("2003-9-19"),
+        precio: [
+            { formato: "tapa blanda", precio: 17.95 },
+            { formato: "kindle", precio: 7.99 }
+        ],
+        paginas: 336
+    },
+    {
+        titulo: "El oro del depredador",
+        coleccion: "Maquinas mortales",
+        escritor: "Philip Reeve",
+        fecha_publicacion: new Date("2003-9-19"),
+        precio: [
+            { formato: "tapa blanda", precio: 17.95 },
+            { formato: "kindle", precio: 7.99 }
+        ],
+        paginas: 336
+    },
+    {
+        titulo: "Inventos infernales",
+        coleccion: "Maquinas mortales",
+        escritor: "Philip Reeve",
+        fecha_publicacion: new Date("2005-3-18"),
+        precio: [
+            { formato: "tapa blanda", precio: 17.95 },
+            { formato: "kindle", precio: 7.99 }
+        ],
+        paginas: 352
+    },
+    {
+        titulo: "Una llanura tenebrosa",
+        coleccion: "Maquinas mortales",
+        escritor: "Philip Reeve",
+        fecha_publicacion: new Date("2006-3-20"),
+        precio: [
+            { formato: "tapa blanda", precio: 17.95 },
+            { formato: "kindle", precio: 7.99 }
+        ],
+        paginas: 544
+    },
+    {
+        titulo: "La Comunidad del Anillo",
+        coleccion: "El Señor de los Anillos",
+        escritor: "J. R. R. Tolkien",
+        fecha_publicacion: new Date("1954-6-29"),
+        precio: [
+            { formato: "tapa dura", precio: 20.95 },
+            { formato: "tapa blanda", precio: 10.95 },
+            { formato: "kindle", precio: 6.72 }
+        ],
+        paginas: 488
+    },
+    {
+        titulo: "Las Dos Torres",
+        coleccion: "El Señor de los Anillos",
+        escritor: "J. R. R. Tolkien",
+        fecha_publicacion: new Date("1954-11-11"),
+        precio: [
+            { formato: "tapa dura", precio: 20.95 },
+            { formato: "tapa blanda", precio: 10.95 },
+            { formato: "kindle", precio: 4.80 }
+        ],
+        paginas: 408
+    },
+    {
+        titulo: "Las Dos Torres",
+        coleccion: "El Señor de los Anillos",
+        escritor: "J. R. R. Tolkien",
+        fecha_publicacion: new Date("1955-8-20"),
+        precio: [
+            { formato: "tapa dura", precio: 20.95 },
+            { formato: "tapa blanda", precio: 10.95 },
+            { formato: "kindle", precio: 4.80 }
+        ],
+        paginas: 520
+    },
 ])
 
 db.escritores.insertMany([
@@ -35,5 +171,16 @@ db.escritores.insertMany([
         nombre: "Brandon Sanderson",
         fecha_nacimiento: new Date("1975-12-19"),
         pais: "Estados Unidos",
-    }
+    },
+    {
+        nombre: "Philip Reeve",
+        fecha_nacimiento: new Date("1966-2-28"),
+        pais: "Reino Unido",
+    },
+    {
+        nombre: "J. R. R. Tolkien",
+        fecha_nacimiento: new Date("1892-1-3"),
+        fecha_muerte: new Date("1973-9-2"),
+        pais: "Estado Libre de Orange",
+    },
 ])
